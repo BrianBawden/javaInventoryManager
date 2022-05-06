a4dec3c6b03be924c876ab1bfb172e5582b73aaa
Brian Bawden <brianbawden001@gmail.com>
2022-05-06 08:50:42 -0400

    Not working!

--- OutdoorInventory.java
+++ OutdoorInventory.java
@@ -2,8 +2,11 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.io.BufferedWriter;
 import java.io.File;  
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 
 public class OutdoorInventory
 
@@ -14,8 +17,9 @@ public class OutdoorInventory
 
     public static void main(String[] args) {// Start main
         // addItem();
-        printFile();
+        // printFile();
         searchFileItemNum();
+        editFile();
     }// end main
 
     /////////////////////// Methods/////////////////////
@@ -42,12 +46,12 @@ public class OutdoorInventory
         } // end catch
     }// end addItem
 
-    // The printFile method is called when you want to print the file in path.
+    // The printFile method is called when you want to print the file.
     static void printFile()
     {//start of printFile
         try{//start try catch
             File objFile = new File(path); //opens file saved in path
-            Scanner objReader = new Scanner(objFile);//gives access to the objFile 
+            Scanner objReader = new Scanner(objFile);//Provides methods to interact with objFile.
             while(objReader.hasNextLine())// iterates through objFile until the end of the file and prints out each line.
             {
                 String data = objReader.nextLine();
@@ -56,46 +60,112 @@ public class OutdoorInventory
             objReader.close();
         } catch(FileNotFoundException e)
         {
-            System.out.println("An error occurred.");
+            System.out.println("File not found.");
             e.printStackTrace();
         }//end try catch
     }// end if printFile
 
+    //The searchFileItemNum method is used to print out the specific information searched for based on the item number.
     static void searchFileItemNum() 
     {
+        //The following variables are set to black because at this point in the method the file has not started searching yet.
         String itemNu = "";
         String itemNa = "";
         String itemQu = "";
         String itemPr = "";
-        boolean found = false;
+        boolean found = false;//found will change to true when the item searched for is found.
 
         System.out.println("Enter item number to search for: ");
         String userSearch = userInput();
 
         try
         {
-            sc = new Scanner(new File(path));
-            sc.useDelimiter("[,\n]");
+            sc = new Scanner(new File(path));// sets sc to the file being searched 
+            sc.useDelimiter("[,\n]");//both , and \n are used ad delimiters because the items are seperated by ,'s and new lines.
 
-                while(sc.hasNext() && !found)
+                while(sc.hasNext() && !found)//The file is searched until the end or if found is changed to true.
                 {
-                    itemNu = sc.next();
-                    itemNa = sc.next();
-                    itemQu = sc.next();
-                    itemPr = sc.next();
+                    itemNu = sc.next();//These variables check the next item in the list.
+                    itemNa = sc.next();//These variables check the next item in the list.
+                    itemQu = sc.next();//These variables check the next item in the list.
+                    itemPr = sc.next();//These variables check the next item in the list.
                 
-                    if(itemNu.equals(userSearch))
+                    if(itemNu.equals(userSearch))//if the itemNu matches what the user is searching for the following is executed and the method will end.
                     {
                         found = true;
-                        System.out.print(itemNu + itemNa + itemQu + itemPr);
+                        System.out.println(itemNu + itemNa + itemQu + itemPr);
                     }
                 }
         }
         catch (Exception e)
         {
-            System.out.println("An error occurred while searching.");
+            System.out.println("Search file failed.");
         }
         
+    }
+
+    static void editFile()//editFile is used by entering the item number to be edited and new information replaces the old information
+    {
+        printFile();// this call to printFile shows the data in the file so the user can see the info to edit.
+
+        String tempFile = "temp.csv;";//A temp file is used to copy the old info to except for the info to be edited then continues copying the file until the end
+        File oldFile = new File(path);//designate path for the file to read from and to be replaced.
+        File newFile = new File(tempFile);// new file that will replace old file after copying information and replacing desired info
+        String itemNu = "";//Starting blank variable for future use.
+        String itemNa = "";//Starting blank variable for future use.
+        String itemQu = "";//Starting blank variable for future use.
+        String itemPr = "";//Starting blank variable for future use.
+
+        System.out.println("\nEnter the item number to edit: ");
+        String editNum = userInput();
+        System.out.println("\nEnter new item number: ");
+        String newNum = userInput();
+        System.out.println("\nEnter new item name: ");
+        String newNam = userInput();
+        System.out.println("\nEnter new item quantity: ");
+        String newQua = userInput();
+        System.out.println("\nEnter new item price: ");
+        String newPri = userInput();
+
+        try
+        {
+            FileWriter fw = new FileWriter(tempFile, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            sc = new Scanner(new File(path));
+            sc.useDelimiter("[,\n]");
+
+
+            while(sc.hasNext())
+            {
+                itemNu = sc.next();
+                itemNa = sc.next();
+                itemQu = sc.next();
+                itemPr = sc.next();
+                if(itemNu.equals(editNum))
+                {
+                   pw.println(newNum + ", " + newNam + ", " + newQua + ", " + newPri);
+                }
+                else
+                {
+                    pw.print(itemNu + "," + itemNa + "," + itemQu + "," + itemPr);
+                }
+            }
+            sc.close();
+            pw.flush();
+            pw.close();
+            oldFile.delete();
+            File dump = new File(path);
+            newFile.renameTo(dump);
+    
+          }
+          catch(Exception e)
+          {
+            System.out.println("Error editing record.");
+
+
+        }
+
     }
 
     //userInput method is used to get string input from user.
